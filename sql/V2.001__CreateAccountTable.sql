c850c1d89ab6e62fbd059dbc7757e8349efd5ce3
Caleb <calebsmith0319@gmail.com>
2025-03-29 17:33:16 -0500

    manually pass null uuid since this isn't going to use the  extension

--- sql/V2.001__CreateAccountTable.sql
+++ sql/V2.001__CreateAccountTable.sql
@@ -15,9 +15,9 @@ CREATE TABLE account (
     preferences_id uuid REFERENCES AccountPreferences(id),
     last_seen timestamp with time zone,
     created_at timestamp with time zone DEFAULT clock_timestamp(),
-    created_by uuid DEFAULT uuid_nil(),
+    created_by uuid DEFAULT '00000000-0000-0000-0000-000000000000',
     updated_at timestamp with time zone DEFAULT clock_timestamp(),
-    updated_by uuid DEFAULT uuid_nil()
+    updated_by uuid DEFAULT '00000000-0000-0000-0000-000000000000'
 );
 
 -- Create system user
@@ -27,6 +27,6 @@ VALUES(
        'e6e3173ec2c327e8d0a5017a7f5d4b9243441654f809eb6a28bfeaccb64f14bd',
        'system@pwap.com',
        null,
-       uuid_nil(),
-       uuid_nil()
+       '00000000-0000-0000-0000-000000000000',
+       '00000000-0000-0000-0000-000000000000'
       )
