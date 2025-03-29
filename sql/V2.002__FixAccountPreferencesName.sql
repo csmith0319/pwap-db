25bef1e4762b818200bcb4047f84888377869248
Caleb <calebsmith0319@gmail.com>
2025-03-29 18:06:11 -0500

    Rename AccountPreferences table to enforce consistency

---
+++ sql/V2.002__FixAccountPreferencesName.sql
@@ -0,0 +1,13 @@
+-- V2.002__FixAccountPreferencesName.sql
+-- Rename AccountPreferences table to account_preferences
+ALTER TABLE "AccountPreferences" RENAME TO account_preferences;
+
+-- Update the foreign key in account table to reference the new table name
+-- First, drop the existing foreign key constraint
+ALTER TABLE account DROP CONSTRAINT IF EXISTS account_preferences_id_fkey;
+
+-- Then, add the correct foreign key constraint
+ALTER TABLE account
+    ADD CONSTRAINT account_preferences_id_fkey
+        FOREIGN KEY (preferences_id) REFERENCES account_preferences(id);
+
